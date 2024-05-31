05ef6dc22c1c909a8a77011d505ae7a0f39c8abe
Dinniatul <kundinnii29@gmail.com>
2024-05-31 18:09:39 +0700

    add field image

--- database/migrations/2024_05_29_100341_create_products_table.php
+++ database/migrations/2024_05_29_100341_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
     Schema::create('products', function (Blueprint $table) {
         $table->id();
         $table->string('product_name');
+        $table->string('image');
         $table->text('description')->nullable();
         $table->decimal('price', 10, 2);
         $table->integer('stock');
